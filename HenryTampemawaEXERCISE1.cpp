ce8678b3dc386760d6f08729305def42012e83f3
glorio <69961033+gloriohenry@users.noreply.github.com>
2021-03-08 14:16:51 +0800

    Add files via upload

---
+++ HenryTampemawaEXERCISE1.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+class karyawan
+{
+	cout << "Program Menyimpan Nomor Karyawan" << endl;
+	cout << "=================================" << endl;
+	cout << endl:
+	
+	string data anggota:
+	char objek;
+	int karyawan 1;
+	int karyawan 2;
+	int karyawan 3;
+	
+	// proese input
+	cout<<"karyawan 1 :";
+	cout<<"karyawan 2 :";
+	cout<<"karyawan 3 :";
+	
+	cout<<"pengguna :";
+	cin>>pengguna;
+	
+	cout<<"jumlah data untuk tiga karyawan: ";
+	cin>>tiga_karyawan;
+	
+	// tentukan data untuk karyawan
+	switch (pengguna){
+		case karyawan1;
+		case karyawan2;
+		case karyawan3;
+	}
+	// proses output
+		cout <<endl;
+		cout<<nama karyawan<< "data karyawan"
+	cout<<endl;
+	
+	return 0;
+}
+
